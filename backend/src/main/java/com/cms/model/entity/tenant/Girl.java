32879d261a979506210b2079e9a372498cc92a22
カン <hchkang8710@gmail.com>
2025-12-31 16:05:24 +0900

    添加订单相关的实体类、数据库变更集及前端页面和组件，实现订单的创建、编辑和展示功能。

---
+++ backend/src/main/java/com/cms/model/entity/tenant/Girl.java
@@ -0,0 +1,26 @@
+package com.cms.model.entity.tenant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "t_girls")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Girl extends BaseEntity {
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Column(name = "status")
+  private String status;
+}
